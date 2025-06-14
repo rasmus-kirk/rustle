92efd745e68cfb750b15a5611f6d8fc4e90c7848
rasmus-kirk <mail@rasmuskirk.com>
2025-06-14 19:10:59 +0200

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::thread::sleep;
 use std::time::{Duration, SystemTime};
 
 // In seconds
-const DEBUG_INTERVAL: u64 = 60;
+const DEBUG_INTERVAL_DEFAULT: u64 = 60;
 
 #[derive(Parser, Debug)]
 #[command(author="Rasmus Kirk", version, about = "Rustle - Keep your digital speakers from sleeping, using low sound signals", long_about = None)]
@@ -24,8 +24,8 @@ struct Args {
     #[arg(short = 'a', long, default_value_t = 0.01)]
     amplitude: f32,
 
-    /// Minutes of undetected sound until the tone plays (0 to ignore playing sound)
-    #[arg(short = 's', long, default_value_t = 0)]
+    /// Minutes of undetected sound until the tone plays
+    #[arg(short = 's', long, default_value_t = 10)]
     minutes_of_silence: u64,
 }
 
@@ -76,6 +76,14 @@ fn main() {
     let args = Args::parse();
     env_logger::init();
 
+    let debug_interval = match std::env::var("DEBUG_INTERVAL") {
+        Ok(val) => val.parse().unwrap_or(DEBUG_INTERVAL_DEFAULT),
+        Err(e) => {
+            error!("{}", e);
+            DEBUG_INTERVAL_DEFAULT
+        }
+    };
+
     let mut silence_start = SystemTime::now();
     let program_start = SystemTime::now();
     loop {
@@ -92,7 +100,7 @@ fn main() {
             silence_start = SystemTime::now();
         }
 
-        if handle_err!(program_start.elapsed()).as_secs() % DEBUG_INTERVAL == 0 {
+        if handle_err!(program_start.elapsed()).as_secs() % debug_interval == 0 {
             if is_playing {
                 debug!("Sound is currently playing")
             } else {
