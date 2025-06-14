77f9c02af54a6758656d22c44d8e1015dd384a8b
rasmus-kirk <mail@rasmuskirk.com>
2025-06-14 13:10:22 +0200

    preparing for release

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ use std::time::{Duration, SystemTime};
 use std::{fs, io};
 
 // In seconds
-const DEBUG_INTERVAL: u64 = 5;
+const DEBUG_INTERVAL: u64 = 60;
 
 #[derive(Parser, Debug)]
 #[command(author="Rasmus Kirk", version, about = "Rustle - Keep your digital speakers from sleeping, using low sound signals", long_about = None)]
 struct Args {
-    /// Duration of each tone in seconds
+    /// Duration of each tone in seconds (0 for continual playback)
     #[arg(short = 'd', long, default_value_t = 120.0)]
     pulse_duration: f32,
 
@@ -29,8 +29,8 @@ struct Args {
     #[arg(short = 'a', long, default_value_t = 0.01)]
     amplitude: f32,
 
-    /// Minutes of undetected sound until the tone plays
-    #[arg(short = 's', long, default_value_t = 10)]
+    /// Minutes of undetected sound until the tone plays (0 to ignore playing sound)
+    #[arg(short = 's', long, default_value_t = 0)]
     minutes_of_silence: u64,
 }
 
@@ -53,74 +53,6 @@ fn is_playing_pipewire() -> anyhow::Result<bool> {
         .output()?
         .stdout;
     Ok(str::from_utf8(&x)?.trim() != "0")
-}
-
-// This function is horrible no matter what I do lol
-fn is_playing_alsa() -> io::Result<bool> {
-    let base_path = Path::new("/proc/asound");
-
-    let handle_err = |e| {
-        error!("{e}");
-        false
-    };
-
-    let f = |entry: io::Result<DirEntry>, starts_with| {
-        let entry = entry.ok()?;
-        let filename_starts_with = entry.file_name().to_string_lossy().starts_with(starts_with);
-        let is_dir = entry
-            .file_type()
-            .map(|ft| ft.is_dir())
-            .unwrap_or_else(handle_err);
-        if !(filename_starts_with && is_dir) {
-            return None;
-        };
-        Some(entry)
-    };
-
-    let check_sub = |path| {
-        fs::read_dir(path)
-            .into_iter()
-            .flatten()
-            .filter(|entry| {
-                entry
-                    .as_ref()
-                    .is_ok_and(|x| x.file_name().to_str().is_some_and(|x| x == "status"))
-            })
-            .any(|status| {
-                status.is_ok_and(|status| {
-                    fs::read_to_string(status.path())
-                        .map(|content| content.contains("state: RUNNING"))
-                        .unwrap_or_else(handle_err)
-                })
-            })
-    };
-
-    let check_pcm = |path| {
-        fs::read_dir(path)
-            .into_iter()
-            .flatten()
-            .filter_map(|x| f(x, "sub"))
-            .any(|sub| check_sub(sub.path()))
-    };
-
-    let check_card = |path: &Path| {
-        fs::read_dir(path)
-            .into_iter()
-            .flatten()
-            .filter_map(|x| f(x, "pcm"))
-            .any(|pcm| check_pcm(pcm.path()))
-    };
-
-    Ok(fs::read_dir(base_path)?
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.file_name().to_string_lossy().starts_with("card"))
-        .filter(|entry| {
-            entry
-                .file_type()
-                .map(|ft| ft.is_dir())
-                .unwrap_or_else(handle_err)
-        })
-        .any(|card| check_card(&card.path())))
 }
 
 fn play_sound(args: &Args) -> anyhow::Result<()> {
